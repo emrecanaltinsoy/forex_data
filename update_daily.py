5c90b29a2a0a71e318142b66c5498899ff68f762
Emrecan Altinsoy <emrecanaltinsoy@yahoo.com.tr>
2021-06-22 14:31:56 +0200

    update daily modified

--- update_daily.py
+++ update_daily.py
@@ -8,32 +8,33 @@ today = datetime.date.today()
 base_url = "https://www.x-rates.com/historical/?from=USD&amount=1&date"
 
 df = pd.read_csv("forex_usd_data.csv")
-last_year, last_month, last_day = [int(val) for val in df.iloc[-1,:]["date(y-m-d)"].split("-")]
+last_year, last_month, last_day = [
+    int(val) for val in df.iloc[-1, :]["date(y-m-d)"].split("-")
+]
 
-year = today.year
-month = today.month
-day = today.day
+date_range = pd.date_range(
+    start=df.iloc[-1, :]["date(y-m-d)"], end=f"{today.year}-{today.month}-{today.day}"
+)
 
-for y in range(last_year, year+1):
-    for m in range(last_month, month+1):
-        m = f"0{m}" if m < 9 else m
-        for d in range(last_day+1, day):
-            d = f"0{d}" if d < 9 else d
-            
-            URL = f"{base_url}={y}-{m}-{d}"
+for dt in date_range[1:-1]:
+    y = dt.year
+    m = f"0{dt.month}" if dt.month <= 9 else dt.month
+    d = f"0{dt.day}" if dt.day <= 9 else dt.day
 
-            page = requests.get(URL)
+    URL = f"{base_url}={y}-{m}-{d}"
 
-            soup = BeautifulSoup(page.content, "html.parser")
+    page = requests.get(URL)
 
-            table = soup.find_all("tr")[12:]
+    soup = BeautifulSoup(page.content, "html.parser")
 
-            currencies = [table[i].text.split("\n")[1:3][0] for i in range(len(table))]
-            currencies.insert(0, "date(y-m-d)")
-            rates = [table[i].text.split("\n")[1:3][1] for i in range(len(table))]
-            rates.insert(0, f"{y}-{m}-{d}")
-            new_row = {currencies[i]: rates[i] for i in range(len(rates))}
+    table = soup.find_all("tr")[12:]
 
-            df = df.append(new_row, ignore_index=True)
+    currencies = [table[i].text.split("\n")[1:3][0] for i in range(len(table))]
+    currencies.insert(0, "date(y-m-d)")
+    rates = [table[i].text.split("\n")[1:3][1] for i in range(len(table))]
+    rates.insert(0, f"{y}-{m}-{d}")
+    new_row = {currencies[i]: rates[i] for i in range(len(rates))}
+
+    df = df.append(new_row, ignore_index=True)
 
 df.to_csv("forex_usd_data.csv", index=None)
